9b0511c05967f2d505fe97f903c0834fa9c69651
Roberto Javier Homs Puron <rhoms@cells.es>
2024-03-04 14:23:20 +0100

    Update macros

--- moco/sardana/macro/moco_utils.py
+++ moco/sardana/macro/moco_utils.py
@@ -134,6 +134,19 @@ def moco_inbeam_conf(self, config):
             return moco.InBeamConf
 
 
+@macro(param_def=[['config', Type.String, Optional,
+                  'Configuration parameter see OUTBEAM/?OUTBEAM command']],
+       result_def=[['config', Type.String, None, 'Current configuration']])
+def moco_outbeam_conf(self, config):
+    if config is not None:
+        with output_context(self, 'Moco setting OutBeamConfig "{}"...'.format(
+                config)):
+            moco.write_attribute('OutBeamConf', config)
+    else:
+        with output_context(self, 'Moco reading OutBeamConfig...'):
+            return moco.InBeamConf
+
+
 @macro(param_def=[['cmd', Type.String, None, 'Command to send']])
 def moco_cmd(self, cmd):
     with output_context(self, 'Moco running OnlineCommand "{}"'.format(
@@ -144,5 +157,114 @@ def moco_cmd(self, cmd):
         return moco.OnlineCmd(cmd)
 
 
+@macro(param_def=[['pos', Type.Float, Optional, 'Position to set']],
+       result_def=[['pos', Type.Float, None, 'Current position']])
+def moco_piezo(self, pos):
+    if pos is not None:
+        with output_context(self, 'Moco setting Piezo "{}"...'.format(
+                pos)):
+            moco.write_attribute('piezo', pos)
+    else:
+        with output_context(self, 'Moco reading Piezo...'):
+            return moco.piezo
 
+
+@macro(param_def=[['freq', Type.Float, Optional, 'Set Frequency']],
+       result_def=[['freq', Type.Float, None, 'Current frequency']])
+def moco_frequency(self, freq):
+    if freq is not None:
+        with output_context(self, 'Moco setting Frequency "{}"...'.format(
+                freq)):
+            moco.write_attribute('Frequency', freq)
+    else:
+        with output_context(self, 'Moco reading Frequency...'):
+            return moco.frequency
+
+
+@macro(param_def=[['phase', Type.Float, Optional, 'Set Phase']],
+       result_def=[['phase', Type.Float, None, 'Current Phase']])
+def moco_phase(self, phase):
+    if phase is not None:
+        with output_context(self, 'Moco setting Phase "{}"...'.format(
+                phase)):
+            moco.write_attribute('Phase', phase)
+    else:
+        with output_context(self, 'Moco reading Phase...'):
+            return moco.phase
+
+
+
+@macro(param_def=[['mode', Type.String, Optional, 'Set Mode']],
+       result_def=[['mode', Type.String, None, 'Current Mode']])
+def moco_mode(self, mode):
+    if mode is not None:
+        with output_context(self, 'Moco setting Mode "{}"...'.format(
+                mode)):
+            moco.write_attribute('Mode', mode)
+    else:
+        with output_context(self, 'Moco reading Mode...'):
+            return moco.mode
+
+
+@macro(param_def=[['softbeam', Type.Float, Optional, 'Set SoftBeam']],
+       result_def=[['softbeam', Type.Float, None, 'Current SoftBeam']])
+def moco_softbeam(self, softbeam):
+    if softbeam is not None:
+        with output_context(self, 'Moco setting SoftBeam "{}"...'.format(
+                softbeam)):
+            moco.write_attribute('Softbeam', softbeam)
+    else:
+        with output_context(self, 'Moco reading Softbeam...'):
+            return moco.mode
+
+
+@macro(param_def=[['speed', Type.Float, Optional, 'Set MoveSpeed']],
+       result_def=[['speed', Type.Float, None, 'Current MoveSpeed']])
+def moco_movespeed(self, speed):
+    if speed is not None:
+        with output_context(self, 'Moco setting MoveSpeed "{}"...'.format(
+                speed)):
+            moco.write_attribute('MoveSpeed', speed)
+    else:
+        with output_context(self, 'Moco reading MoveSpeed...'):
+            return moco.movespeed
+
+
+@macro(param_def=[['speed', Type.Float, Optional, 'Set ScanSpeed']],
+       result_def=[['speed', Type.Float, None, 'Current ScanSpeed']])
+def moco_scanspeed(self, speed):
+    if speed is not None:
+        with output_context(self, 'Moco setting ScanSpeed "{}"...'.format(
+                speed)):
+            moco.write_attribute('ScanSpeed', speed)
+    else:
+        with output_context(self, 'Moco reading ScanSpeed...'):
+            return moco.scanspeed
+
+
+@macro(param_def=[['flags', [['flag', Type.String, None, 'flags'],
+                   {'min': 0}], None, 'List of flags']],
+       result_def=[['flags', Type.String, None, 'Current list of flags '
+                                                'separated by spaces']])
+def moco_operationflags(self, flags):
+    if flags:
+        with output_context(self, 'Moco setting flags "{}"...'.format(
+                flags)):
+            moco.setoperationflags(flags)
+    else:
+        with output_context(self, 'Moco reading flags...'):
+            flags = moco.operationflags
+            if flags:
+                flags = ' '.join(flags)
+            else:
+                flags = ''
+            return flags
+
+
+@macro(param_def=[['flags', [['flag', Type.String, None, 'flags'],
+                   {'min': 1}], None, 'List of flags']])
+def moco_clear_operationflags(self, flags):
+    with output_context(self, 'Moco clear flags "{}"...'.format(
+                flags)):
+            moco.clearoperationflags(flags)
 
