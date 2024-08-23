90207ee6d3f7a92bfd8d32c5a719890b1c213310
Roberto Javier Homs Puron <rhoms@cells.es>
2024-08-23 13:12:30 +0200

    Bump version 2.2.0 to 2.3.0

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ALBA-Synchrotron/MoCo',
-    version='2.2.0',
+    version='2.3.0',
     zip_safe=False,
 )
 
